9a00b89b6994340de5a547aaa1d4d01f0891f9c0
manueleyno <manuel.eyno@gmail.com>
2017-04-18 15:52:17 +0000

    pre Herouk

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :stories
+  get 'about', to: 'pages#about'
+  get 'impressum', to: 'pages#impressum'
+  #resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
