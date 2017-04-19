0fbb16b9bb4a8523e401fae4f43833e0a55abd8b
manueleyno <manuel.eyno@gmail.com>
2017-04-19 14:15:54 +0000

    Added Mailer Settings to Enviorment File

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'impressum', to: 'pages#impressum'
   resources :contacts, only: :create
-  get 'kontakt', to: 'contacts#new', as: 'new_contact'
+  get 'Kontakt_Formular', to: 'contacts#new', as: 'new_contact'
 end
