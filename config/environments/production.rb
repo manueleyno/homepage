b5bd38ad81d41895c54e19e45a9bed84e7f47217
manueleyno <manuel.eyno@gmail.com>
2017-05-18 14:45:42 +0000

    porduction.rb geändert wegen background img

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,9 +22,11 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  # Do not fallback to assets pipeline if a precompiled asset is missed. //AUF TRUE wegen Background IMG geaendert
+  config.assets.compile = true
+  # Wegen Background IMG reingesetzt und auf true
+  config.serve_static_assets = true
+  
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
