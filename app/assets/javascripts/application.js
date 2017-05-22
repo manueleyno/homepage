d5bfce3f3cfc48dd9aadc19537d3a97e4c0550d4
manueleyno <manuel.eyno@gmail.com>
2017-05-22 19:58:42 +0000

    fb und instagram updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,14 +19,6 @@
 //= require cookies_eu
 
 
-  (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.8";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));//= require cookies_eu
-
 document.addEventListener("turbolinks:load", function() {
   tinymce.remove();
   tinymce.init({
